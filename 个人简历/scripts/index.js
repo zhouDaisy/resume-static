f140c60f42828f4c38cf1b6e12bc2b7fc7c8bc72
zhouDaisy <zmlovezd@icloud.com>
2016-07-09 20:55:10 +0800

    change
    is better

--- 个人简历/scripts/index.js
+++ 个人简历/scripts/index.js
@@ -9,39 +9,36 @@ $(document).ready(function(){
      $(".selected").click(function(){
         $("#books").css("display","block");
         $("#code").css("display","none");
-        $("#display").css("display","none");
         $(".null").css("display","block");
         $(".null2").css("display","none");
-        $(".null3").css("display","none");
         $(".tool").css("background-color","#fe6b40");
-        $(".display").css("background-color","#fe6b40");
         $(this).css("background-color","#a6a6a6");
      });
      $(".tool").click(function(){
         $(".null2").css("display","block");
         $(".null").css("display","none");
-        $(".null3").css("display","none");
         $("#code").css("display","block");
         $("#books").css("display","none");
-        $("#display").css("display","none");
         $(".selected").css("background-color","#fe6b40");
-        $(".display").css("background-color","#fe6b40");
         $(this).css("background-color","#a6a6a6");
      })
-     $(".display").click(function(){
-        $(".null3").css("display","block");
-        $(".null2").css("display","none");
-        $(".null").css("display","none");
-        $("#code").css("display","none");
-        $("#books").css("display","none");
-        $("#display").css("display","block");
-        $(".selected").css("background-color","#fe6b40");
-        $(".tool").css("background-color","#fe6b40");
-        $(this).css("background-color","#a6a6a6");
-     });
+     //作品展示
+      $("#production_list nav ul li").each(function(index){
+        var liNode = $(this);
+        liNode.hover(function(){
+        timeroutid = setTimeout(function(){
+            $("#production_message .selected").removeClass("selected");
+            $("#production_message .production_message").eq(index).addClass("selected");
+        },300);     
+    });
+    $("#production_content").mouseleave(function(){
+        clearTimeout(timeroutid);
+       $("#production_message .selected").removeClass("selected");  
+    })  
+    });
       //轮播图图片定义
-   var imgsNode=$("#display_imgs li"),
-   pagesNode=$("#display_pages li");
+   var imgsNode=$("#production_imgs li"),
+   pagesNode=$("#production_pages li");
    imgsNode.eq(0).show().siblings().hide();
    //手动焦点
    pagesNode.each(function(index){
@@ -73,7 +70,7 @@ $(document).ready(function(){
         imgsNode.eq(i).fadeIn(300).siblings().fadeOut(300);
         pagesNode.eq(i).addClass("selected").siblings().removeClass("selected");
     }
-   $("#display_loop,#display_finger").hover(function(){
+   $("#production_loop,#production_finger").hover(function(){
         clearInterval(time);},function(){
         time=setInterval(autoplay,7000);
    });
